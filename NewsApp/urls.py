dc164a2d48b9ac0c0279c2f52b45b79756f8daee
unknown <devanshshah180800@gmail.com>
2021-06-08 18:33:52 +0530

    pages for categories

--- NewsApp/urls.py
+++ NewsApp/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-    
+    path('general',views.general,name='general'),
+    path('sports',views.sports,name='sports'),
+    path('politics',views.politics,name='politics'),
+    path('entertainment',views.entertainment,name='entertainment'),
+    path('music',views.music,name='music'),
+    path('technology',views.technology,name='technology'),
+    path('science',views.science,name='science'),
+    path('business',views.business,name='business'),
+    path('health',views.health,name='health'),
 ]
--- NewsApp/views.py
+++ NewsApp/views.py
@@ -16,3 +16,39 @@ def index(request):
     # return render(request,'index.html',{'data':data})
     top=requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
     return render(request,'index.html',{'top':top})
+
+def general(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'general.html',{'general':general})
+
+def music(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&category=music&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'music.html',{'music':general})
+
+def sports(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'sports.html',{'sports':general})
+
+def entertainment(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'entertainment.html',{'entertainment':general})
+
+def technology(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'technology.html',{'technology':general})
+
+def science(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'science.html',{'science':general})
+
+def politics(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&category=politics&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'politics.html',{'politics':general})
+
+def health(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'health.html',{'health':general})
+
+def business(request):
+    general=requests.get('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9289ff62266d4b389cacb6d84f70d28f').json()
+    return render(request,'business.html',{'business':general})
